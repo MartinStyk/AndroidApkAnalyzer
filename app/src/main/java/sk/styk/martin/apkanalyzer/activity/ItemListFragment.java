6b7549a6af60ac83f27d069dda78d4c76b13078a
Martin Styk <martin.styk@gmail.com>
2017-06-15 12:08:17 +0200

    RadioButton to choose type of application - xml baseline

--- app/src/main/java/sk/styk/martin/apkanalyzer/activity/ItemListFragment.java
+++ app/src/main/java/sk/styk/martin/apkanalyzer/activity/ItemListFragment.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,10 +23,11 @@ import sk.styk.martin.apkanalyzer.business.task.ItemListLoadTask;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ItemListFragment extends Fragment implements ItemListLoadTask.OnTaskCompleted {
+public class ItemListFragment extends Fragment implements ItemListLoadTask.OnTaskCompleted, View.OnClickListener {
 
     private SimpleItemRecyclerViewAdapter adapter;
     private RecyclerView recyclerView;
+    private View listContainerView;
     private ProgressBar loadingBar;
 
     @Override
@@ -35,6 +38,13 @@ public class ItemListFragment extends Fragment implements ItemListLoadTask.OnTas
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_item_list, container, false);
+
+        // Set the onClick for each of our views as the one implemented by this Fragment
+        view.findViewById(R.id.radio_all_apps).setOnClickListener(this);
+        view.findViewById(R.id.radio_system_apps).setOnClickListener(this);
+        view.findViewById(R.id.radio_user_apps).setOnClickListener(this);
+
+        listContainerView = view.findViewById(R.id.item_list_container);
         recyclerView = (RecyclerView) view.findViewById(R.id.item_list);
         loadingBar = (ProgressBar) view.findViewById(R.id.item_list_loading);
         adapter = new SimpleItemRecyclerViewAdapter(getActivity(), new ArrayList<ApplicationInfo>());
@@ -51,6 +61,25 @@ public class ItemListFragment extends Fragment implements ItemListLoadTask.OnTas
     public void onTaskCompleted(List<ApplicationInfo> applicationInfoList) {
         adapter.dataChange(applicationInfoList);
         loadingBar.setVisibility(View.GONE);
-        recyclerView.setVisibility(View.VISIBLE);
+        listContainerView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Handle click events from radio button
+     */
+    @Override
+    public void onClick(View v) {
+        boolean checked = ((RadioButton) v).isChecked();
+        switch (v.getId()) {
+
+            case R.id.radio_all_apps:
+                Toast.makeText(getActivity(), "All", Toast.LENGTH_LONG).show();
+
+            case R.id.radio_system_apps:
+                Toast.makeText(getActivity(), "System", Toast.LENGTH_LONG).show();
+
+            case R.id.radio_user_apps:
+                Toast.makeText(getActivity(), "user", Toast.LENGTH_LONG).show();
+        }
     }
 }
