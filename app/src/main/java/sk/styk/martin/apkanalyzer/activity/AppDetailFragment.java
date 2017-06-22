2c2a63fc9ae4e153c48caf7cbe6607b5ac0ab6b8
Martin Styk <martin.styk@gmail.com>
2017-06-22 08:43:09 +0200

    Swipe tabs instead of bottom navigation in app detail screen

--- app/src/main/java/sk/styk/martin/apkanalyzer/activity/AppDetailFragment.java
+++ app/src/main/java/sk/styk/martin/apkanalyzer/activity/AppDetailFragment.java
@@ -1,16 +1,11 @@
 package sk.styk.martin.apkanalyzer.activity;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.support.v4.view.ViewPager;
-import android.support.v4.widget.NestedScrollView;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -28,8 +23,7 @@ import sk.styk.martin.apkanalyzer.model.AppBasicInfo;
  * in two-pane mode (on tablets) or a {@link AppDetailActivity}
  * on handsets.
  */
-public class AppDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<AppBasicInfo>,
-        BottomNavigationView.OnNavigationItemSelectedListener, ViewPager.OnPageChangeListener {
+public class AppDetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<AppBasicInfo> {
     /**
      * The fragment argument representing the item ID that this fragment
      * represents.
@@ -44,7 +38,6 @@ public class AppDetailFragment extends Fragment implements LoaderManager.LoaderC
     private AppDetailAdapter adapter;
     private ProgressBar loadingBar;
     private ViewPager viewPager;
-    private BottomNavigationView bottomNavigationView;
     private MenuItem prevMenuItem;
 
     @Override
@@ -63,11 +56,7 @@ public class AppDetailFragment extends Fragment implements LoaderManager.LoaderC
         appBarLayout = (CollapsingToolbarLayout) getActivity().findViewById(sk.styk.martin.apkanalyzer.R.id.toolbar_layout);
         appBarLayuotImageView = (ImageView) getActivity().findViewById(R.id.toolbar_layout_image);
 
-        bottomNavigationView = (BottomNavigationView) rootView.findViewById(R.id.bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(this);
-
         viewPager = (ViewPager) rootView.findViewById(R.id.pager);
-        viewPager.addOnPageChangeListener(this);
         viewPager.setAdapter(adapter);
 
         if (data != null) {
@@ -91,7 +80,6 @@ public class AppDetailFragment extends Fragment implements LoaderManager.LoaderC
         }
         loadingBar.setVisibility(View.GONE);
         viewPager.setVisibility(View.VISIBLE);
-        bottomNavigationView.setVisibility(View.VISIBLE);
     }
 
     @Override
@@ -99,40 +87,4 @@ public class AppDetailFragment extends Fragment implements LoaderManager.LoaderC
         this.data = null;
     }
 
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_item1:
-                viewPager.setCurrentItem(0);
-                break;
-            case R.id.action_item2:
-                viewPager.setCurrentItem(1);
-                break;
-            case R.id.action_item3:
-                viewPager.setCurrentItem(2);
-                break;
-        }
-        return true;
-    }
-
-    @Override
-    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        // nothing to de here
-    }
-
-    @Override
-    public void onPageSelected(int position) {
-        if (prevMenuItem != null) {
-            prevMenuItem.setChecked(false);
-        } else {
-            bottomNavigationView.getMenu().getItem(0).setChecked(false);
-        }
-        bottomNavigationView.getMenu().getItem(position).setChecked(true);
-        prevMenuItem = bottomNavigationView.getMenu().getItem(position);
-    }
-
-    @Override
-    public void onPageScrollStateChanged(int state) {
-        // nothing to de here
-    }
 }
