721905f07fda69c3e5ac69b92850583ddc307e1a
Martin Styk <martin.styk@gmail.com>
2017-07-06 15:37:10 +0200

    RecyclerView in files detail fragment

--- app/src/main/java/sk/styk/martin/apkanalyzer/activity/detailfragment/AppDetailFragment_File.java
+++ app/src/main/java/sk/styk/martin/apkanalyzer/activity/detailfragment/AppDetailFragment_File.java
@@ -2,18 +2,21 @@ package sk.styk.martin.apkanalyzer.activity.detailfragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
-import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import sk.styk.martin.apkanalyzer.R;
 import sk.styk.martin.apkanalyzer.activity.AppDetailFragment;
+import sk.styk.martin.apkanalyzer.adapter.FileListAdapter;
 import sk.styk.martin.apkanalyzer.model.FileData;
-import sk.styk.martin.apkanalyzer.model.ServiceData;
+import sk.styk.martin.apkanalyzer.util.EndlessRecyclerViewOnScrollListener;
 
 /**
  * Created by Martin Styk on 30.06.2017.
@@ -26,11 +29,29 @@ public class AppDetailFragment_File extends Fragment {
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(
-                R.layout.fragment_app_detail_page1, container, false);
+                R.layout.fragment_app_detail_files, container, false);
         FileData data = getArguments().getParcelable(AppDetailFragment.ARG_CHILD);
-        textView = (WebView) rootView.findViewById(R.id.item_detail);
-        String string = String.format("<html><body> %s </body></html>", data.toString());
-        textView.loadData(string, "text/html; charset=utf-8", "utf-8");
+
+
+        RecyclerView rvItems = (RecyclerView) rootView.findViewById(R.id.files_recycler_view);
+        final List<String> allContacts = new ArrayList<String>(data.getAllHashes().keySet());
+        final FileListAdapter adapter = new FileListAdapter(allContacts.subList(0, 50));
+        rvItems.setAdapter(adapter);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        rvItems.setLayoutManager(linearLayoutManager);
+        rvItems.addOnScrollListener(new EndlessRecyclerViewOnScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+                List<String> moreContacts = allContacts.subList(totalItemsCount, totalItemsCount + 50);
+                int curSize = adapter.getItemCount();
+                adapter.appendItems(moreContacts);
+                adapter.notifyItemRangeInserted(curSize, adapter.getItemCount() - 1);
+            }
+        });
         return rootView;
     }
+
 }
+
+
+
---
+++ app/src/main/java/sk/styk/martin/apkanalyzer/adapter/FileListAdapter.java
@@ -0,0 +1,68 @@
+package sk.styk.martin.apkanalyzer.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import sk.styk.martin.apkanalyzer.R;
+
+/**
+ * Created by Martin Styk on 06.07.2017.
+ */
+public class FileListAdapter extends RecyclerView.Adapter<FileListAdapter.ViewHolder> {
+
+    private List<String> allItems = new ArrayList<>();
+    private Context context;
+    private boolean initialLoad = true;
+
+    public FileListAdapter(List<String> initialData) {
+        allItems.addAll(initialData);
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        this.context = parent.getContext();
+        View view = LayoutInflater.from(this.context).inflate(R.layout.list_item_fragment_app_detail_files, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, final int position) {
+        if (allItems != null && position < allItems.size()) {
+            holder.filePath.setText(allItems.get(position));
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return allItems == null ? 0 : allItems.size();
+    }
+
+    public void appendItems(List<String> items) {
+        int oldItemCount = getItemCount();
+        allItems.addAll(items);
+        notifyItemRangeInserted(oldItemCount, items.size());
+
+    }
+
+
+    public void clear() {
+        allItems.clear();
+        notifyDataSetChanged();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        final TextView filePath;
+
+        ViewHolder(View v) {
+            super(v);
+            filePath = (TextView) v.findViewById(R.id.item_detail_file);
+        }
+    }
+}
