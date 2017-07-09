d68134a7616bd7430a40e26b20a8e9ccbcd782fb
Martin Styk <martin.styk@gmail.com>
2017-07-09 10:33:50 +0200

    Remove default fragment

--- app/src/main/java/sk/styk/martin/apkanalyzer/activity/MainActivity.java
+++ app/src/main/java/sk/styk/martin/apkanalyzer/activity/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import sk.styk.martin.apkanalyzer.R;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     public static boolean mTwoPane;
@@ -19,21 +21,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(sk.styk.martin.apkanalyzer.R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = (Toolbar) findViewById(sk.styk.martin.apkanalyzer.R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(sk.styk.martin.apkanalyzer.R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, sk.styk.martin.apkanalyzer.R.string.navigation_drawer_open, sk.styk.martin.apkanalyzer.R.string.navigation_drawer_close);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(sk.styk.martin.apkanalyzer.R.id.nav_view);
-        navigationView.setCheckedItem(sk.styk.martin.apkanalyzer.R.id.nav_camera);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        if (findViewById(sk.styk.martin.apkanalyzer.R.id.item_detail_container) != null) {
+        if (findViewById(R.id.item_detail_container) != null) {
             // The detail container view will be present only in the
             // large-screen layouts (res/values-w900dp).
             // If this view is present, then the
@@ -45,15 +46,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         // only on first run redirect to default fragment
         if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.nav_gallery);
             FragmentManager fragmentManager = getSupportFragmentManager();
-            DefaultFragment fragment = new DefaultFragment();
-            fragmentManager.beginTransaction().replace(sk.styk.martin.apkanalyzer.R.id.first_container, fragment).commit();
+            Fragment fragment = new AppListFragment();
+            fragmentManager.beginTransaction().replace(R.id.first_container, fragment).commit();
         }
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(sk.styk.martin.apkanalyzer.R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -63,14 +65,14 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(sk.styk.martin.apkanalyzer.R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case sk.styk.martin.apkanalyzer.R.id.action_settings:
+            case R.id.action_settings:
                 //TODO start settings activity gere
                 return true;
         }
@@ -85,26 +87,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Fragment fragment = null;
         FragmentManager fragmentManager = getSupportFragmentManager();
 
-        if (id == sk.styk.martin.apkanalyzer.R.id.nav_camera) {
-            // Handle the camera action
-            fragment = new DefaultFragment();
-            fragmentManager.beginTransaction().replace(sk.styk.martin.apkanalyzer.R.id.first_container, fragment).commit();
-
-        } else if (id == sk.styk.martin.apkanalyzer.R.id.nav_gallery) {
+        if (id == R.id.nav_gallery) {
             fragment = new AppListFragment();
-            fragmentManager.beginTransaction().replace(sk.styk.martin.apkanalyzer.R.id.first_container, fragment).commit();
+            fragmentManager.beginTransaction().replace(R.id.first_container, fragment).commit();
 
-        } else if (id == sk.styk.martin.apkanalyzer.R.id.nav_slideshow) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == sk.styk.martin.apkanalyzer.R.id.nav_manage) {
+        } else if (id == R.id.nav_manage) {
 
-        } else if (id == sk.styk.martin.apkanalyzer.R.id.nav_share) {
+        } else if (id == R.id.nav_share) {
 
-        } else if (id == sk.styk.martin.apkanalyzer.R.id.nav_send) {
+        } else if (id == R.id.nav_send) {
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(sk.styk.martin.apkanalyzer.R.id.drawer_layout);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
