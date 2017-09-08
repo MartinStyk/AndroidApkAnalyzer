6465a9d3ea9697d6be60c8bcb5868239aace858b
Martin Styk <martin.styk@gmail.com>
2017-09-08 12:56:58 +0200

    Swap detail fragments position

--- app/src/main/java/sk/styk/martin/apkanalyzer/adapter/AppDetailAdapter.java
+++ app/src/main/java/sk/styk/martin/apkanalyzer/adapter/AppDetailAdapter.java
@@ -28,7 +28,6 @@ import sk.styk.martin.apkanalyzer.model.detail.AppDetailData;
 
 public class AppDetailAdapter extends FragmentStatePagerAdapter {
 
-    private Fragment[] fragments = new Fragment[getCount()];
     private Context context;
     private AppDetailData data;
 
@@ -53,44 +52,45 @@ public class AppDetailAdapter extends FragmentStatePagerAdapter {
                 break;
 
             case 2:
+                args.putParcelable(AppDetailFragment.ARG_CHILD, data.getResourceData());
+                fragment = new AppDetailFragment_Resource();
+                break;
+
+            case 3:
                 args.putParcelableArrayList(AppDetailFragment.ARG_CHILD, (ArrayList<? extends Parcelable>) data.getActivityData());
                 fragment = new AppDetailFragment_Activity();
                 break;
 
-            case 3:
+            case 4:
                 args.putParcelableArrayList(AppDetailFragment.ARG_CHILD, (ArrayList<? extends Parcelable>) data.getServiceData());
                 fragment = new AppDetailFragment_Service();
                 break;
 
-            case 4:
+            case 5:
                 args.putParcelableArrayList(AppDetailFragment.ARG_CHILD, (ArrayList<? extends Parcelable>) data.getContentProviderData());
                 fragment = new AppDetailFragment_Provider();
                 break;
 
-            case 5:
+            case 6:
                 args.putParcelableArrayList(AppDetailFragment.ARG_CHILD, (ArrayList<? extends Parcelable>) data.getBroadcastReceiverData());
                 fragment = new AppDetailFragment_Receiver();
                 break;
 
-            case 6:
+            case 7:
                 args.putStringArrayList(AppDetailFragment.ARG_CHILD, (ArrayList<String>) data.getPermissionData().getUsesPermissions());
                 fragment = new AppDetailFragment_Permission();
                 break;
 
-            case 7:
+            case 8:
                 args.putStringArrayList(AppDetailFragment.ARG_CHILD,(ArrayList<String>) data.getPermissionData().getDefinesPermissions());
                 fragment = new AppDetailFragment_Permission();
                 break;
 
-            case 8:
+            case 9:
                 args.putParcelable(AppDetailFragment.ARG_CHILD, data.getFileData());
                 fragment = new AppDetailFragment_File();
                 break;
 
-            case 9:
-                args.putParcelable(AppDetailFragment.ARG_CHILD, data.getResourceData());
-                fragment = new AppDetailFragment_Resource();
-                break;
             default:
                 return null;
         }
@@ -111,21 +111,21 @@ public class AppDetailAdapter extends FragmentStatePagerAdapter {
             case 1:
                 return context.getResources().getString(R.string.certificate);
             case 2:
-                return context.getResources().getString(R.string.activities);
-            case 3:
-                return context.getResources().getString(R.string.services);
-            case 4:
-                return context.getResources().getString(R.string.content_providers);
-            case 5:
-                return context.getResources().getString(R.string.broadcast_receivers);
-            case 6:
-                return context.getResources().getString(R.string.permissions);
-            case 7:
-                return context.getResources().getString(R.string.defined_permissions);
-            case 8:
-                return context.getResources().getString(R.string.files);
-            case 9:
                 return context.getResources().getString(R.string.resources);
+            case 3:
+                return context.getResources().getString(R.string.activities);
+            case 4:
+                return context.getResources().getString(R.string.services);
+            case 5:
+                return context.getResources().getString(R.string.content_providers);
+            case 6:
+                return context.getResources().getString(R.string.broadcast_receivers);
+            case 7:
+                return context.getResources().getString(R.string.permissions);
+            case 8:
+                return context.getResources().getString(R.string.defined_permissions);
+            case 9:
+                return context.getResources().getString(R.string.files);
         }
         return "TODO";
     }
