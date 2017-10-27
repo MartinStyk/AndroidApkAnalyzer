8c4d03f4ffd9066d5d5d055819919483ca88cc19
Martin Styk <martin.styk@gmail.com>
2017-10-27 15:24:38 +0200

    Install APK after analysis

--- app/src/main/java/sk/styk/martin/apkanalyzer/activity/OnInstallAppDetailActivity.java
+++ app/src/main/java/sk/styk/martin/apkanalyzer/activity/OnInstallAppDetailActivity.java
@@ -1,22 +1,36 @@
 package sk.styk.martin.apkanalyzer.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.NavUtils;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
+import java.io.File;
+
 import sk.styk.martin.apkanalyzer.R;
 import sk.styk.martin.apkanalyzer.util.ApkFilePicker;
+import sk.styk.martin.apkanalyzer.util.GenericFileProvider;
 
-public class OnInstallAppDetailActivity extends AppCompatActivity {
+public class OnInstallAppDetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int REQUEST_STORAGE_PERMISSIONS = 987;
+
+    private String apkPath;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,49 +39,69 @@ public class OnInstallAppDetailActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
 
-        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
-
-        if(getIntent().getData() == null){
-            Toast.makeText(this, "Can not get APK data.", Toast.LENGTH_LONG).show();
+        if (getIntent().getData() == null) {
+            Toast.makeText(this, getString(R.string.error_loading_package_detail), Toast.LENGTH_LONG).show();
             setResult(RESULT_CANCELED);
             finish();
         }
 
-        if (savedInstanceState == null) {
+        intent = getIntent();
 
-            Bundle arguments = new Bundle();
-            arguments.putString(AppDetailFragment.ARG_PACKAGE_PATH, ApkFilePicker.getPathFromIntentData(getIntent(), this));
-            Fragment detailFragment = new AppDetailFragment();
-            detailFragment.setArguments(arguments);
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.item_detail_container, detailFragment, AppDetailFragment.TAG)
-                    .commit();
+        if (savedInstanceState == null) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                    && Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_STORAGE_PERMISSIONS);
+            } else {
+                setupDetailFragment();
+            }
         }
 
         FloatingActionButton actionButton = (FloatingActionButton) findViewById(R.id.btn_actions);
+        actionButton.setVisibility(View.GONE);
 
-        // this happens only in tablet mode when this activity is rotated from horizontal to vertical orientation
-        if (actionButton == null) {
-            appBarLayout.setExpanded(false);
-        } else {
-            actionButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    //delegate to fragment
-                    ((AppDetailFragment) getSupportFragmentManager().findFragmentByTag(AppDetailFragment.TAG)).onClick(view);
-                }
-            });
-        }
-
+        FloatingActionButton installButton = (FloatingActionButton) findViewById(R.id.btn_install);
+        installButton.setVisibility(View.VISIBLE);
+        installButton.setOnClickListener(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpTo(this, new Intent(this, AppListActivity.class));
-                return true;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_STORAGE_PERMISSIONS) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                setupDetailFragment();
+            } else {
+                Snackbar.make(findViewById(android.R.id.content), R.string.permission_not_granted, Snackbar.LENGTH_LONG).show();
+            }
         }
-        return super.onOptionsItemSelected(item);
     }
+
+    private void setupDetailFragment() {
+        Bundle arguments = new Bundle();
+        apkPath = ApkFilePicker.getPathFromIntentData(intent, this);
+        arguments.putString(AppDetailFragment.ARG_PACKAGE_PATH, apkPath);
+        Fragment detailFragment = new AppDetailFragment();
+        detailFragment.setArguments(arguments);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.item_detail_container, detailFragment, AppDetailFragment.TAG)
+                .commit();
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        Uri apkUri = null;
+        try{
+            apkUri = FileProvider.getUriForFile(this, GenericFileProvider.AUTHORITY, new File(apkPath));
+        } catch (Exception e){
+            Log.e(OnInstallAppDetailActivity.class.getSimpleName(), e.getLocalizedMessage());
+            Toast.makeText(this, getString(R.string.install_failed), Toast.LENGTH_LONG).show();
+            return;
+        }
+        intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        startActivity(intent);
+        finish();
+    }
+
 }
---
+++ app/src/main/java/sk/styk/martin/apkanalyzer/util/GenericFileProvider.java
@@ -0,0 +1,10 @@
+package sk.styk.martin.apkanalyzer.util;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Martin Styk on 27.10.2017.
+ */
+public class GenericFileProvider extends FileProvider {
+        public static final String AUTHORITY = "sk.styk.martin";
+}
