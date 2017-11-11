d560eab69976b39904ff7c7048d93988757907cb
Martin Styk <martin.styk@gmail.com>
2017-11-11 20:24:53 +0100

    Serializa date in better format

--- app/src/main/java/sk/styk/martin/apkanalyzer/util/JsonSerializationUtils.java
+++ app/src/main/java/sk/styk/martin/apkanalyzer/util/JsonSerializationUtils.java
@@ -1,6 +1,9 @@
 package sk.styk.martin.apkanalyzer.util;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.text.DateFormat;
 
 /**
  * Created by Martin Styk on 06.11.2017.
@@ -10,7 +13,7 @@ public class JsonSerializationUtils {
     private Gson gson;
 
     public JsonSerializationUtils() {
-        this.gson = new Gson();
+        this.gson = new GsonBuilder().setDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz").create();
     }
 
     public String serialize(Object object) {
