b0989c83816ec9136e39c025c4b8027a52ae095b
Martin Styk <martin.styk@gmail.com>
2018-01-03 19:26:48 +0100

    Do not start upload task when intro is not finished

--- app/src/main/java/sk/styk/martin/apkanalyzer/ApkAnalyzer.java
+++ app/src/main/java/sk/styk/martin/apkanalyzer/ApkAnalyzer.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.os.StrictMode;
 
 import sk.styk.martin.apkanalyzer.business.task.upload.MultipleAppDataUploadService;
+import sk.styk.martin.apkanalyzer.util.FirstStartHelper;
 
 /**
  * Created by Martin Styk on 30.10.2017.
@@ -17,6 +18,7 @@ public class ApkAnalyzer extends Application {
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
 
-        MultipleAppDataUploadService.start(getApplicationContext());
+        if (!FirstStartHelper.isFirstStart(getApplicationContext()))
+            MultipleAppDataUploadService.start(getApplicationContext());
     }
 }
