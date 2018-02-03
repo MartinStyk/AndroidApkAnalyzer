3f39402d4edee8bb19c0e600fa90902c45432d23
Martin Styk <martin.styk@gmail.com>
2018-02-03 20:55:09 +0100

    Correct URL

--- app/src/main/java/sk/styk/martin/apkanalyzer/util/networking/ServerUrls.kt
+++ app/src/main/java/sk/styk/martin/apkanalyzer/util/networking/ServerUrls.kt
@@ -6,8 +6,8 @@ package sk.styk.martin.apkanalyzer.util.networking
  */
 object ServerUrls {
 
-    const val URL_BASE = "http://192.168.1.32:3000";
-//    const val URL_BASE = "https://apk-analyzer.herokuapp.com"
+//    const val URL_BASE = "http://192.168.1.32:3000";
+    const val URL_BASE = "https://apk-analyzer.herokuapp.com"
     const val GENERATE_200 = "/available"
     const val UPLOAD_RECORD_PATH = "/app_records"
     const val REPACKAGED_DETECTION_PATH = "/repackaged_detection"
