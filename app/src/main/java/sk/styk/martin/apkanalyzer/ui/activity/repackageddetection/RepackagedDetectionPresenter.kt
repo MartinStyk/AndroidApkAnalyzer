928b3a28118e2a30df704c985bca5c1fd71a7a8b
Martin Styk <martin.styk@gmail.com>
2018-03-01 15:06:50 +0100

    Repackaged detection results visualisaton
    Show pie charts with ratio of signatures
    
    Show texts explaining the results

--- app/src/main/java/sk/styk/martin/apkanalyzer/ui/activity/repackageddetection/RepackagedDetectionPresenter.kt
+++ app/src/main/java/sk/styk/martin/apkanalyzer/ui/activity/repackageddetection/RepackagedDetectionPresenter.kt
@@ -2,10 +2,18 @@ package sk.styk.martin.apkanalyzer.ui.activity.repackageddetection
 
 import android.os.Bundle
 import android.support.v4.app.LoaderManager
+import android.support.v4.content.ContextCompat
 import android.support.v4.content.Loader
+import lecho.lib.hellocharts.model.PieChartData
+import lecho.lib.hellocharts.model.SliceValue
+import sk.styk.martin.apkanalyzer.ApkAnalyzer.Companion.context
+import sk.styk.martin.apkanalyzer.R
 import sk.styk.martin.apkanalyzer.business.base.task.ApkAnalyzerAbstractAsyncLoader
 import sk.styk.martin.apkanalyzer.business.upload.task.RepackagedDetectionLoader
+import sk.styk.martin.apkanalyzer.model.server.RepackagedDetectionResult
 import sk.styk.martin.apkanalyzer.model.server.RepackagedDetectionStatus
+import java.math.BigDecimal
+import java.util.*
 
 
 /**
@@ -41,9 +49,9 @@ class RepackagedDetectionPresenter(
         when (result) {
             is RepackagedDetectionLoader.LoaderResult.Success -> {
                 when (result.result.status) {
-                    RepackagedDetectionStatus.NOK -> view.showAppNotOk(result.result)
-                    RepackagedDetectionStatus.OK -> view.showAppOk(result.result)
-                    RepackagedDetectionStatus.INSUFFICIENT_DATA -> view.showAppNotDetected(result.result)
+                    RepackagedDetectionStatus.NOK -> view.showAppNotOk(result.result, generateAppSignatureChart(result.result), generateMajoritySignatureChart(result.result))
+                    RepackagedDetectionStatus.OK -> view.showAppOk(result.result, generateAppSignatureChart(result.result), generateMajoritySignatureChart(result.result))
+                    RepackagedDetectionStatus.INSUFFICIENT_DATA -> view.showAppNotDetected(result.result, generateAppSignatureChart(result.result), generateMajoritySignatureChart(result.result))
                 }
             }
             is RepackagedDetectionLoader.LoaderResult.NotConnectedToInternet -> view.showNoInternetConnection()
@@ -54,4 +62,52 @@ class RepackagedDetectionPresenter(
     }
 
     override fun onLoaderReset(loader: Loader<RepackagedDetectionLoader.LoaderResult>?) {}
+
+    private fun generateAppSignatureChart(result: RepackagedDetectionResult): PieChartData {
+
+//        val percentageSameSignature = result.percentageSameSignature
+        val percentageSameSignature = BigDecimal.TEN
+
+        val values = ArrayList<SliceValue>(2)
+
+        val sliceInteresting = SliceValue(0f, ContextCompat.getColor(context, R.color.accent))
+        sliceInteresting.setLabel(context.getString(R.string.repackaged_result_detection_app_signature_same_dev,percentageSameSignature.toString ()))
+        sliceInteresting.setTarget(percentageSameSignature.toFloat())
+
+        val sliceRest = SliceValue(100f, ContextCompat.getColor(context, R.color.primary_light))
+        sliceRest.setTarget(100 - percentageSameSignature.toFloat())
+        sliceRest.setLabel(context.getString(R.string.repackaged_result_detection_app_signature_other_devs, BigDecimal(100).minus(percentageSameSignature).toString()))
+
+        values.add(sliceInteresting)
+        values.add(sliceRest)
+
+        val data = PieChartData(values)
+        data.setHasCenterCircle(true)
+        data.setHasLabels(true)
+        data.setCenterCircleScale(0.5f)
+        return data
+    }
+
+    private fun generateMajoritySignatureChart(result: RepackagedDetectionResult): PieChartData {
+        val percentageMajoritySignature = BigDecimal.valueOf(25)
+
+        val values = ArrayList<SliceValue>(2)
+
+        val sliceInteresting = SliceValue(0f, ContextCompat.getColor(context, R.color.accent))
+        sliceInteresting.setLabel(context.getString(R.string.repackaged_result_detection_majority_signature_most_common_devs, percentageMajoritySignature.toString ()))
+        sliceInteresting.setTarget(percentageMajoritySignature.toFloat())
+
+        val sliceRest = SliceValue(100f, ContextCompat.getColor(context, R.color.primary_light))
+        sliceRest.setTarget(100 - percentageMajoritySignature.toFloat())
+        sliceRest.setLabel(context.getString(R.string.repackaged_result_detection_majority_signature_other_devs, BigDecimal(100).minus(percentageMajoritySignature).toString()))
+
+        values.add(sliceInteresting)
+        values.add(sliceRest)
+
+        val data = PieChartData(values)
+        data.setHasCenterCircle(true)
+        data.setHasLabels(true)
+        data.setCenterCircleScale(0.5f)
+        return data
+    }
 }
