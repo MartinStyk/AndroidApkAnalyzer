617ff3833996a6f993c13167381ac0f510ee5532
Martin Styk <martin.styk@gmail.com>
2018-12-25 19:33:23 +0100

    Anima fab

--- app/src/main/java/sk/styk/martin/apkanalyzer/ui/activity/applist/searchable/AppListBindingAdapters.kt
+++ app/src/main/java/sk/styk/martin/apkanalyzer/ui/activity/applist/searchable/AppListBindingAdapters.kt
@@ -1,6 +1,7 @@
 package sk.styk.martin.apkanalyzer.ui.activity.applist.searchable
 
 import android.databinding.BindingAdapter
+import android.support.design.widget.FloatingActionButton
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.RecyclerView
 
@@ -18,6 +19,16 @@ object AppListBindingAdapters {
             recyclerView.setHasFixedSize(true)
             recyclerView.addItemDecoration(DividerItemDecoration(recyclerView.context, DividerItemDecoration.VERTICAL))
             recyclerView.adapter = adapter
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("show")
+    fun bindFabIsShown(fab: FloatingActionButton, show: Boolean?) {
+        if (show == true && !fab.isShown) {
+            fab.show()
+        } else if (show == false && fab.isShown) {
+            fab.hide()
         }
     }
 
